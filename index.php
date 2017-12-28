aba72d83e6aa30ddf3654eabed1492d9d19ca038
kadeska <kadeska23@gmail.com>
2017-12-28 20:46:00 +0000

    modified:   index.php 	modified:   style/css_file.css

--- index.php
+++ index.php
@@ -5,29 +5,6 @@
 <link rel="stylesheet" type="text/css" href="style/css_file.css">
 </head>
 <body>
-//<?php echo phpinfo(); ?/>
-
-<div class="text">
-  <svg>
-   <defs>
-     <mask id="mask" x="0" y="0" width="100%" height="100%" >
-       <!-- alpha rectangle -->
-       <!-- rectángulo alfa -->
-       <rect id="alpha" x="0" y="0" width="100%" height="100%"/>
-       <!-- All text that you want -->
-
       <h1>Welcome to the Mortuus Terra website</h1>
-
-       <text id="title" x="50%" y="0" dy="1.58em">SVG + CSS</text>
-       <text id="subtitle" x="50%" y="0" dy="9.8em">Use it please!</text>
-     </mask>
-    </defs>
-    <!-- Apply color here! -->
-    <rect id="base" x="0" y="0" width="100%" height="100%"/>
-  </svg>
-</div>
-
-<section class="intro"></section>
-
 </body>
 </html>
