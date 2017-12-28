0d106e62b82e591adc5ea775c4d6ffe5e8f839a2
kadeska <kadeska23@gmail.com>
2017-12-28 20:29:21 +0000

    modified:   index.php 	deleted:    style/css_file 	modified:   style/css_file.css

--- index.php
+++ index.php
@@ -5,12 +5,29 @@
 <link rel="stylesheet" type="text/css" href="style/css_file.css">
 </head>
 <body>
-<h1>Welcome to the Mortuus Terra website</h1>
-<p>This is some text</p>
+//<?php echo phpinfo(); ?/>
 
-<p>This is some more text</p>
+<div class="text">
+  <svg>
+   <defs>
+     <mask id="mask" x="0" y="0" width="100%" height="100%" >
+       <!-- alpha rectangle -->
+       <!-- rectángulo alfa -->
+       <rect id="alpha" x="0" y="0" width="100%" height="100%"/>
+       <!-- All text that you want -->
 
-<p>Here is all of the PHP info</p>
-<?php echo phpinfo(); ?>
+      <h1>Welcome to the Mortuus Terra website</h1>
+
+       <text id="title" x="50%" y="0" dy="1.58em">SVG + CSS</text>
+       <text id="subtitle" x="50%" y="0" dy="9.8em">Use it please!</text>
+     </mask>
+    </defs>
+    <!-- Apply color here! -->
+    <rect id="base" x="0" y="0" width="100%" height="100%"/>
+  </svg>
+</div>
+
+<section class="intro"></section>
+
 </body>
 </html>
