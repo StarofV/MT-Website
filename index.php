f22d5629422ceefb90b9a021cd9b055f6e882c0a
kadeska <kadeska23@gmail.com>
2017-12-28 19:59:04 +0000

    modified:   index.php 	new file:   style/css_file 	new file:   style/css_file.css

--- index.php
+++ index.php
@@ -1,26 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Home!</title>
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-</style>
+<title>Mortuus Terra</title>
+<link rel="stylesheet" type="text/css" href="style/css_file.css">
 </head>
 <body>
-<h1>Welcome to nginx!</h1>
-<p>If you see this page, the nginx web server is successfully installed and
-working. Further configuration is required.</p>
+<h1>Welcome to the Mortuus Terra website</h1>
+<p>This is some text</p>
 
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a>.<br/>
-Commercial support is available at
-<a href="http://nginx.com/">nginx.com</a>.</p>
+<p>This is some more text</p>
 
-<p><em>Thank you for using nginx.</em></p>
+<p>Here is all of the PHP info</p>
 <?php echo phpinfo(); ?>
 </body>
 </html>
